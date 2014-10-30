295fc865721d5d4a8eabd8cc2a2e45c42e32cc1d
Corentin Lecouvey <corentin.lecouvey@open.eurogiciel.org>
2014-10-30 11:00:15 +0100

    Adapt bluetooth popup replies to NTB
    Change-Id: If3d1181966a3256e763bc62ecd33898a8bbfab1b
    Signed-off-by: Corentin Lecouvey <corentin.lecouvey@open.eurogiciel.org>

--- plugins/wlmessage.c
+++ plugins/wlmessage.c
@@ -1,54 +1,13 @@
 #include <bundle.h>
-#include <dbus/dbus.h>
-#include <dbus/dbus-glib.h>
-#include <dbus/dbus-glib-lowlevel.h>
+#include <bluetooth.h>
 #include <dlog.h>
 #include <notification.h>
 #include <libwlmessage.h>
 
-typedef enum {
-	BT_AGENT_ACCEPT,
-	BT_AGENT_REJECT,
-	BT_AGENT_CANCEL,
-	BT_CORE_AGENT_TIMEOUT,
-} bt_agent_accept_type_t;
-
-
-static DBusGProxy* create_agent_proxy(const char *path)
-{
-	DBusGConnection *sys_conn;
-
-	sys_conn = dbus_g_bus_get(DBUS_BUS_SYSTEM, NULL);
-	if (sys_conn == NULL) {
-		LOGD("ERROR: Can't get on system bus");
-		return NULL;
-	}
-
-	return dbus_g_proxy_new_for_name(sys_conn, "org.projectx.bt", path,
-			"org.bluez.Agent1");
-}
-
-static DBusGProxy* create_obex_proxy(void)
-{
-	DBusGConnection *sys_conn;
-
-	sys_conn = dbus_g_bus_get(DBUS_BUS_SYSTEM, NULL);
-	if (sys_conn == NULL) {
-		LOGD("ERROR: Can't get on system bus");
-		return NULL;
-	}
-
-	return dbus_g_proxy_new_for_name(sys_conn, "org.bluez.frwk_agent",
-			"/org/obex/ops_agent", "org.openobex.Agent");
-}
-
-static void send_user_reply (DBusGProxy *agent_proxy,
-							 const char *reply_method,
-							 bt_agent_accept_type_t user_reply)
-{
-	dbus_g_proxy_call_no_reply( agent_proxy, reply_method, G_TYPE_UINT,
-								user_reply, G_TYPE_INVALID, G_TYPE_INVALID);
-}
+#define POPUP_TYPE_INFO "user_info_popup"
+#define POPUP_TYPE_USERCONFIRM "user_confirm_popup"
+#define POPUP_TYPE_USERPROMPT "user_agreement_popup"
+#define REGISTER_PAIRING_AGENT_TITLE "register_pairing_agent"
 
 int display_notification (notification_h noti)
 {
@@ -66,68 +25,29 @@ int display_notification (notification_h noti)
 
 	LOGD("NOTIFICATION RECEIVED: %s - %s - %s", pkgname, title, content);
 
-	if (!strcasecmp(pkgname, "bluetooth-frwk-bt-service")) {
-		bundle *user_data = NULL;
-		DBusGProxy *proxy;
-		char *reply_method;
-		int button = 0;
+	if (!strcasecmp(pkgname, "bt-agent")) {
+		enum { NOTIF_TYPE_INFO, NOTIF_TYPE_USERPROMPT, NOTIF_TYPE_USERCONFIRM } type = 0;
+		int clicked_button = 0;
+		char *info1 = NULL;
 
-		const char *device_name = NULL;
-		const char *passkey = NULL;
-		const char *file = NULL;
-		const char *agent_path;
-		const char *event_type = NULL;
-
-		notification_get_execute_option(noti, NOTIFICATION_EXECUTE_TYPE_SINGLE_LAUNCH,NULL, &user_data);
-
-		event_type = bundle_get_val(user_data, "event-type");
-		if (!event_type) {
-			LOGD("Not a bluetooth-related notification...");
-		} else {
-			LOGD("bluetooth notification type: [%s]", event_type);
+		if (!strcasecmp(REGISTER_PAIRING_AGENT_TITLE, title)) {
+			LOGD("Register pairing agent. Do not display this popup.");
+			bt_initialize();
+			bt_agent_register_sync();
+			return 1;
 		}
 
-		if (!strcasecmp(event_type, "passkey-confirm-request")) {
-			device_name = (gchar*) bundle_get_val(user_data, "device-name");
-			passkey = (gchar*) bundle_get_val(user_data, "passkey");
-			agent_path = bundle_get_val(user_data, "agent-path");
-			proxy = create_agent_proxy(agent_path);
-			reply_method = "ReplyConfirmation";
-
-		} else if(!strcasecmp(event_type, "pin-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "passkey-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "passkey-display-request")) {
-			LOGD("Not implemented");
-
-		} else if (!strcasecmp(event_type, "authorize-request")) {
-			device_name = (gchar*) bundle_get_val(user_data, "device-name");
-			agent_path = bundle_get_val(user_data, "agent-path");
-			proxy = create_agent_proxy(agent_path);
-			reply_method = "ReplyAuthorize";
-
-		} else if(!strcasecmp(event_type, "app-confirm-request")) {
-			LOGD("Not implemented");
-
-		} else if(!strcasecmp(event_type, "push-authorize-request")) {
-			file = (gchar*) bundle_get_val(user_data, "file");
-			device_name = (gchar*) bundle_get_val(user_data, "device-name");
-			proxy = create_obex_proxy();
-			reply_method = "ReplyAuthorize";
-
-		} else if(!strcasecmp(event_type, "confirm-overwrite-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "keyboard-passkey-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "bt-information")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "exchange-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "phonebook-request")) {
-			LOGD("Not implemented");
-		} else if(!strcasecmp(event_type, "message-request")) {
-			LOGD("Not implemented");
+		notification_get_text (noti, NOTIFICATION_TEXT_TYPE_INFO_1, &info1);
+		if (info1) {
+			if (!strcasecmp(POPUP_TYPE_INFO, info1)) {
+				type = NOTIF_TYPE_INFO;
+			}
+			else if (!strcasecmp(POPUP_TYPE_USERCONFIRM, info1)) {
+				type = NOTIF_TYPE_USERCONFIRM;
+			}
+			else if (!strcasecmp(POPUP_TYPE_USERPROMPT, info1)) {
+				type = NOTIF_TYPE_USERPROMPT;
+			}
 		}
 
 		struct wlmessage *wlmessage = wlmessage_create ();
@@ -137,16 +57,21 @@ int display_notification (notification_h noti)
 		wlmessage_add_button (wlmessage, 1, "Yes");
 		wlmessage_add_button (wlmessage, 0, "No");
 
-		button = wlmessage_show (wlmessage, NULL);
-		if (button < 0) {
+		clicked_button = wlmessage_show (wlmessage, NULL);
+
+		if (clicked_button < 0) {
 			wlmessage_destroy (wlmessage);
-			return 0;
-		} else if (button == 1) {
+			return;
+		} else if ((clicked_button == 1) && (type == NOTIF_TYPE_USERCONFIRM)) {
 			LOGD("user clicked on 'Yes' popup button");
-			send_user_reply(proxy, reply_method, BT_AGENT_ACCEPT);
-		} else if (button == 0) {
+			if (!strcmp(pkgname, "bt-agent")) {
+				bt_agent_reply_sync(0);
+			}
+		} else if ((clicked_button == 0) && (type == NOTIF_TYPE_USERCONFIRM)) {
 			LOGD("user clicked on 'No' popup button");
-			send_user_reply(proxy, reply_method, BT_AGENT_CANCEL);
+			if (!strcmp(pkgname, "bt-agent")) {
+				bt_agent_reply_sync(1);
+			}
 		}
 		wlmessage_destroy (wlmessage);
 		return 1;
