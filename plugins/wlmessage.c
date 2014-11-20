1bd25d4ecfd7be1c7955473b77dbea36692a4e9b
Corentin Lecouvey <corentin.lecouvey@open.eurogiciel.org>
2014-11-20 17:03:04 +0100

    handle opp agent and receive file popup
    Change-Id: Iae3810cf96916bf07aa0235fa6ffa3bdd81e6cad
    Signed-off-by: Corentin Lecouvey <corentin.lecouvey@open.eurogiciel.org>

--- plugins/wlmessage.c
+++ plugins/wlmessage.c
@@ -3,11 +3,68 @@
 #include <dlog.h>
 #include <notification.h>
 #include <libwlmessage.h>
+#include <stdlib.h>
 
 #define POPUP_TYPE_INFO "user_info_popup"
 #define POPUP_TYPE_USERCONFIRM "user_confirm_popup"
 #define POPUP_TYPE_USERPROMPT "user_agreement_popup"
 #define REGISTER_PAIRING_AGENT_TITLE "register_pairing_agent"
+#define REGISTER_OPP_AGENT_TITLE "register_opp_agent"
+#define BLUETOOTH_ICON_PATH	"/usr/share/icons/default/bt-icon.png"
+
+static void bt_opp_server_transfer_progress_cb_test(const char *file,
+							long long size,
+							int percent,
+							void *user_data)
+{
+	LOGD("file = %s / size = %lld / percent = %d", file, size, percent);
+
+}
+
+static void bt_opp_server_transfer_finished_cb_test(int result,
+						const char *file,
+						long long size,
+						void *user_data)
+{
+	LOGD("file = %s / size = %lld / result = %d", file, size, result);
+
+}
+
+void bt_opp_server_push_requested_cb_test(const char *file, int size, void *user_data)
+{
+	int transfer_id;
+	int clicked_button = 0;
+	LOGD("file = %s / size = %d", file, size);
+
+	int len;
+	char *content;
+
+	len = snprintf(NULL, 0, "Do you want to accept the received file: %s (size: %d) ?", file, size);
+	content = malloc(len + 1);
+	snprintf(content, len, "Do you want to accept the received file: %s (size: %d) ?", file, size);
+
+	struct wlmessage *wlmessage = wlmessage_create ();
+	wlmessage_set_title (wlmessage, "Bluetooth file transfer");
+	wlmessage_set_icon (wlmessage, BLUETOOTH_ICON_PATH);
+	wlmessage_set_message (wlmessage, content);
+	wlmessage_add_button (wlmessage, 1, "Yes");
+	wlmessage_add_button (wlmessage, 0, "No");
+
+	clicked_button = wlmessage_show (wlmessage, NULL);
+
+	if (clicked_button < 0) {
+		wlmessage_destroy (wlmessage);
+		return;
+	} else if (clicked_button == 1) {
+		LOGD("user clicked on 'Yes' popup button");
+		bt_opp_server_accept(bt_opp_server_transfer_progress_cb_test,
+				bt_opp_server_transfer_finished_cb_test, file, NULL, &transfer_id);
+	} else if (clicked_button == 0){
+		LOGD("user clicked on 'No' popup button");
+		bt_opp_server_reject();
+	}
+	wlmessage_destroy (wlmessage);
+}
 
 int display_notification (notification_h noti)
 {
@@ -34,6 +91,13 @@ int display_notification (notification_h noti)
 			LOGD("Register pairing agent. Do not display this popup.");
 			bt_initialize();
 			bt_agent_register_sync();
+
+			return 1;
+		}
+
+		if (!strcasecmp(REGISTER_OPP_AGENT_TITLE, title)) {
+			LOGD("Register opp agent. Do not display this popup.");
+			bt_opp_server_initialize("/tmp", bt_opp_server_push_requested_cb_test, NULL);
 			return 1;
 		}
 
